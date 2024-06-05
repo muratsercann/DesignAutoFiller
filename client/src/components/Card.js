87132f49aba28675f7506bcac22ce594e4879898
Murat Sercan <muratsercann@gmail.com>
2024-06-06 02:11:23 +0300

    mouse and key events added

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,12 +4,10 @@ import backgroundImage from "./background.png";
 export default function Card({ settings, setSettings }) {
   const itemRef = useRef(null);
   const [isImageLoaded, setIsImageLoaded] = useState(false);
-  useEffect(() => {
-    console.log("card.js is executed");
-  });
-  useEffect(() => {
-    console.log("setting have been changed : ", settings);
-  }, [settings]);
+  const [selectedDiv, setSelectedDiv] = useState(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const dragStart = useRef({ x: 0, y: 0 });
+  const initialTranslate = useRef({ x: 0, y: 0 });
 
   useLayoutEffect(() => {
     const rotationAngle = settings?.rotate || 0;
@@ -17,7 +15,6 @@ export default function Card({ settings, setSettings }) {
     const verAlign = settings?.verticalAlignment || "";
     const translateX = settings?.translateX || 0;
     const translateY = settings?.translateY || 0;
-
     if (itemRef.current && isImageLoaded) {
       const newTranslateX = calculateTranslateX(
         itemRef.current,
@@ -49,6 +46,107 @@ export default function Card({ settings, setSettings }) {
   const translateY = settings?.translateY ?? 0;
   const rotationAngle = settings?.rotate ?? 0;
 
+  const handleClick = (e) => {
+    setSelectedDiv(e.target);
+  };
+
+  const handleNonSelectedClick = (e) => {
+    setSelectedDiv(null);
+  };
+
+  const handleKeyDown = (e) => {
+    if (selectedDiv) {
+      const step = 5;
+      let newTranslateX;
+      let newTranslateY;
+
+      switch (e.key) {
+        case "ArrowUp":
+          newTranslateY = settings.translateY - step;
+          setSettings({
+            ...settings,
+            translateY: newTranslateY,
+            verticalAlignment: "",
+          });
+          break;
+        case "ArrowDown":
+          newTranslateY = settings.translateY + step;
+          setSettings({
+            ...settings,
+            translateY: newTranslateY,
+            verticalAlignment: "",
+          });
+          break;
+        case "ArrowLeft":
+          newTranslateX = settings.translateX - step;
+          setSettings({
+            ...settings,
+            translateX: newTranslateX,
+            horizontalAlignment: "",
+          });
+          break;
+        case "ArrowRight":
+          newTranslateX = settings.translateX + step;
+          setSettings({
+            ...settings,
+            translateX: newTranslateX,
+            horizontalAlignment: "",
+          });
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  const handleMouseDown = (e) => {
+    setSelectedDiv(e.target);
+    setIsDragging(true);
+
+    dragStart.current = { x: e.clientX, y: e.clientY };
+
+    initialTranslate.current = {
+      x: settings.translateX,
+      y: settings.translateY,
+    };
+    e.preventDefault();
+  };
+
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      const dx = e.clientX - dragStart.current.x;
+      const dy = e.clientY - dragStart.current.y;
+
+      console.log(`(dx,dy) : ${dx},${dy}`);
+
+      const newTranslateX = initialTranslate.current.x + dx;
+      const newTranslateY = initialTranslate.current.y + dy;
+
+      setSettings({
+        ...settings,
+        translateX: newTranslateX,
+        translateY: newTranslateY,
+        horizontalAlignment: "",
+        verticalAlignment: "",
+      });
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
   return (
     <div style={{ width: 250, position: "relative", marginTop: "150px" }}>
       <div>
@@ -57,6 +155,7 @@ export default function Card({ settings, setSettings }) {
           className="img-fluid"
           alt=""
           onLoad={() => setIsImageLoaded(true)}
+          onClick={handleNonSelectedClick}
         />
 
         {isImageLoaded && (
@@ -64,10 +163,15 @@ export default function Card({ settings, setSettings }) {
             ref={itemRef}
             style={{
               transform: `translate(${translateX}px, ${translateY}px) rotate(${rotationAngle}deg)`,
+              border:
+                selectedDiv === itemRef.current ? "2px dotted #a686ff" : "none",
+              cursor: "move",
             }}
             className="outer"
+            onClick={handleClick}
+            onMouseDown={handleMouseDown}
           >
-            <div className="header">DAVETLİSİNİZ</div>
+            Hello :)
           </div>
         )}
       </div>
