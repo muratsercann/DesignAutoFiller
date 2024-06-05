8b71cbfa67eb33e5df0793ef1cc3553ee30dfc35
Murat Sercan <muratsercann@gmail.com>
2024-06-05 23:33:03 +0300

    code review

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -5,16 +5,11 @@ export default function Card({ formData, setFormData }) {
   const itemRef = useRef(null);
   const [isImageLoaded, setIsImageLoaded] = useState(false);
   useEffect(() => {
-    console.log("car.js is executed");
+    console.log("card.js is executed");
   });
   useEffect(() => {
     console.log("formData is changed in Card.js : ", formData);
   }, [formData]);
-
-  // let now = performance.now();
-  // while (performance.now() - now < 100) {
-  //   // Do nothing for a bit...
-  // }
 
   useLayoutEffect(() => {
     const rotationAngle = formData?.rotate || 0;
@@ -23,77 +18,28 @@ export default function Card({ formData, setFormData }) {
     const translateX = formData?.translateX || 0;
     const translateY = formData?.translateY || 0;
 
-    function getRotatedStyle(
-      element,
-      angle,
-      horAlign,
-      verAlign,
-      translateX,
-      translateY
-    ) {
-      let data = {
-        translateX: 0,
-        translateY: 0,
-      };
-
-      const rad = (Math.abs(angle) * Math.PI) / 180;
-      const sin = Math.sin(rad);
-      const cos = Math.cos(rad);
-
-      const centerX = element.offsetWidth / 2;
-      const centerY = element.offsetHeight / 2;
-
-      const parentWidth = element.offsetParent.offsetWidth;
-      const parentHeight = element.offsetParent.offsetHeight;
-
-      if (horAlign === "Left") {
-        translateX =
-          Math.abs(cos * centerX) + Math.abs(sin * centerY) - centerX;
-      } else if (horAlign === "Right") {
-        translateX =
-          centerX - (Math.abs(cos * centerX) + Math.abs(sin * centerY));
-        translateX += parentWidth - element.offsetWidth;
-      } else if (horAlign === "Center") {
-        translateX = (parentWidth - element.offsetWidth) / 2;
-      }
-
-      if (verAlign === "Top") {
-        translateY =
-          Math.abs(sin * centerX) + Math.abs(cos * centerY) - centerY;
-        translateY -= parentHeight;
-      } else if (verAlign === "Bottom") {
-        translateY =
-          centerY - (Math.abs(sin * centerX) + Math.abs(cos * centerY));
-        translateY -= element.offsetHeight;
-      } else if (verAlign === "Center") {
-        translateY = 0;
-        translateY -= (parentHeight + element.offsetHeight) / 2;
-      }
-
-      data.translateX = translateX;
-      data.translateY = translateY;
-
-      return data;
-    }
-
     if (itemRef.current && isImageLoaded) {
-      const newStyle = getRotatedStyle(
+      const newTranslateX = calculateTranslateX(
         itemRef.current,
         rotationAngle,
         horAlign,
+        translateX
+      );
+      const newTranslateY = calculateTranslateY(
+        itemRef.current,
+        rotationAngle,
         verAlign,
-        translateX,
         translateY
       );
 
       if (
-        newStyle.translateX !== formData.translateX ||
-        newStyle.translateY !== formData.translateY
+        newTranslateX !== formData.translateX ||
+        newTranslateY !== formData.translateY
       ) {
         setFormData({
           ...formData,
-          translateX: newStyle.translateX,
-          translateY: newStyle.translateY,
+          translateX: newTranslateX,
+          translateY: newTranslateY,
         });
       }
     }
@@ -127,4 +73,68 @@ export default function Card({ formData, setFormData }) {
       </div>
     </div>
   );
+}
+
+function calculateTranslateX(
+  element,
+  rotationAngle,
+  horizantalAlignment,
+  translateX
+) {
+  let newTranslateX = translateX;
+
+  const rad = (Math.abs(rotationAngle) * Math.PI) / 180;
+  const sin = Math.sin(rad);
+  const cos = Math.cos(rad);
+
+  const centerX = element.offsetWidth / 2;
+  const centerY = element.offsetHeight / 2;
+
+  const parentWidth = element.offsetParent.offsetWidth;
+
+  //Horizontal
+  if (horizantalAlignment === "Left") {
+    newTranslateX = Math.abs(cos * centerX) + Math.abs(sin * centerY) - centerX;
+  } else if (horizantalAlignment === "Right") {
+    newTranslateX =
+      centerX - (Math.abs(cos * centerX) + Math.abs(sin * centerY));
+    newTranslateX += parentWidth - element.offsetWidth;
+  } else if (horizantalAlignment === "Center") {
+    newTranslateX = (parentWidth - element.offsetWidth) / 2;
+  }
+
+  return newTranslateX;
+}
+
+function calculateTranslateY(
+  element,
+  rotationAngle,
+  verticalAlignment,
+  translateY
+) {
+  let newTranslateY = translateY;
+
+  const rad = (Math.abs(rotationAngle) * Math.PI) / 180;
+  const sin = Math.sin(rad);
+  const cos = Math.cos(rad);
+
+  const centerX = element.offsetWidth / 2;
+  const centerY = element.offsetHeight / 2;
+
+  const parentHeight = element.offsetParent.offsetHeight;
+
+  //Vertical
+  if (verticalAlignment === "Top") {
+    newTranslateY = Math.abs(sin * centerX) + Math.abs(cos * centerY) - centerY;
+    newTranslateY -= parentHeight;
+  } else if (verticalAlignment === "Bottom") {
+    newTranslateY =
+      centerY - (Math.abs(sin * centerX) + Math.abs(cos * centerY));
+    newTranslateY -= element.offsetHeight;
+  } else if (verticalAlignment === "Center") {
+    newTranslateY = 0;
+    newTranslateY -= (parentHeight + element.offsetHeight) / 2;
+  }
+
+  return newTranslateY;
 }
