fafb6432eeb0367eb6cef2cfe864c348fba0f06c
Murat Sercan <muratsercann@gmail.com>
2024-06-10 19:15:46 +0300

    client_v2

---
+++ client_v2/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="header">
+        <h1>Welcome</h1>
+        <p>Let Start with Your Design</p>
+      </div>
+      <div className="home-content"></div>
+    </div>
+  );
+}
