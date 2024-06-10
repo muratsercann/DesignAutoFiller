334e135153f2a0b111b2b1e6292be4217ac3328f
Murat Sercan <muratsercann@gmail.com>
2024-06-10 22:21:33 +0300

    router

--- client_v2/src/App.js
+++ client_v2/src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import Home from "./components/Home";
+import { Router, Route, BrowserRouter, Routes } from "react-router-dom";
+import Resize from "./components/Resize";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/resize" element={<Resize />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client_v2/src/components/Resize/index.js
@@ -0,0 +1,5 @@
+import "./Resize.css";
+
+export default function Resize({}) {
+  return <>Resize your image</>;
+}
