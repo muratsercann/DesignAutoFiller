67667ca25df2a0c7da7cc6ab933de84689c0675a
Murat Sercan <muratsercann@gmail.com>
2024-06-15 00:13:21 +0300

    ..

--- client_v2/src/App.js
+++ client_v2/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./components/Home";
 import { Router, Route, BrowserRouter, Routes } from "react-router-dom";
 import Resize from "./components/Resize";
+import Edit from "./components/Edit";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/resize" element={<Resize />} />
+        <Route exact path="/edit" element={<Edit />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client_v2/src/components/Edit/index.js
@@ -0,0 +1,7 @@
+export default function Edit() {
+  return (
+    <div className="edit">
+      <p>This is edit page..</p>
+    </div>
+  );
+}
