19209b4ae8fca90f838fe92a16050e8320278a02
Murat Sercan <muratsercann@gmail.com>
2024-07-17 19:55:22 +0300

    ..

--- client/src/components/Help.js
+++ client/src/components/Help.js
@@ -1,4 +1,7 @@
 export default function Help() {
+  const strongStyle = {
+    color: "#1bb88b",
+  };
   return (
     <article style={{ color: "var(--bs-gray-400)", fontWeight: "400" }}>
       <p>
@@ -6,36 +9,52 @@ export default function Help() {
         txt files.
       </p>
       <p>
-        <strong>Step 1 :</strong> Upload an image. (The allowed max size is 1024
-        KB.)
+        <div>
+          <strong style={strongStyle}>Step 1 :</strong>
+        </div>{" "}
+        Upload an image. (The allowed max size is 1024 KB.)
       </p>
       <p>
-        <strong>Step 2 :</strong> Add text items to your design and customize
-        them.
+        <div>
+          <strong style={strongStyle}>Step 2 :</strong>
+        </div>{" "}
+        Add text items to your design and customize them.
       </p>
       <p>
-        <strong>Step 3 :</strong> Import a datasource as an xlsx, csv, json, txt
-        file for dublication.
+        <div>
+          <strong style={strongStyle}>Step3 :</strong>
+        </div>{" "}
+        Import a datasource as an xlsx, csv, json, txt file for dublication.
         <p>Note: The file should have a header row.</p>
       </p>
       <p>
-        <strong>Step 4 :</strong> Bind columns from your data source to the text
-        items you added.
+        <div>
+          <strong style={strongStyle}>Step 4 :</strong>
+        </div>{" "}
+        Bind columns from your data source to the text items you added.
       </p>
 
       <p>
-        <strong>Step 5 :</strong> Select the 'Print' from the menu in the left
-        side and adjust size for design that will be dublicated using the data
-        source and will be printed later.
+        <div>
+          <strong style={strongStyle}>Step 5 :</strong>
+        </div>{" "}
+        Select the 'Print' from the menu in the left side and adjust size for
+        design that will be dublicated using the data source and will be printed
+        later.
       </p>
 
       <p>
-        <strong>Step 6 :</strong> After adjusting the size, click 'Print' button
-        and see your browser's print screen with the dublicated design.
+        <div>
+          <strong style={strongStyle}>Step 6 :</strong>
+        </div>{" "}
+        After adjusting the size, click 'Print' button and see your browser's
+        print screen with the dublicated design.
       </p>
       <p>
-        <strong>Finally :</strong> Congratulations! Now, you have dublicated
-        design.
+        <div>
+          <strong style={strongStyle}>Finally :</strong>
+        </div>{" "}
+        Congratulations! Now, you have dublicated design.
       </p>
     </article>
   );
